cd82e7da05583b65c333591283233263fe00e6ee
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-10-13 10:06:50 +0900

    AppLoading

---
+++ App.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { AppLoading, Font, Icon } from "expo";
+
+export default class App extends React.Component {
+  state = {
+    loadCompleted: false
+  };
+  render() {
+    const { loadCompleted } = this.state;
+    if (loadCompleted) {
+      return <View />;
+    } else {
+      return (
+        <AppLoading
+          startAsync={this._loadAssets}
+          onError={this._handleLoadingError}
+          onFinish={this._handleFinishLoading}
+        />
+      );
+    }
+  }
+  _loadAssets = async () => {
+    return Promise.all([
+      Font.loadAsync({
+        ...Icon.Ionicons.font
+      })
+    ]);
+  };
+
+  _handleLoadingError = error => {
+    console.warn(error);
+  };
+
+  _handleFinishLoading = () => {
+    this.setState({ loadCompleted: true });
+  };
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
